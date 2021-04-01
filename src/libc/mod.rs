eaa461745294caf05a5d3998f58c7a7ac70d1ebc
sammyne <lixiangmin01@baidu.com>
2021-04-01 08:28:07 +0000

    docs: add some notes

--- src/libc/mod.rs
+++ src/libc/mod.rs
@@ -7,15 +7,18 @@ pub type c_int = i32;
 pub type c_uint = u32;
 pub type ssize_t = isize;
 
+// Adding link breaks compilation.
+// #[link(name = "utils", kind = "static")]
+// 
+// This APIs comes from libutils.a built by optee_os. It seems ok without linking utils.
+// Just linking the static libraries built on this library to the final TA will succeed.
+// Currently, I have no idea of the underlying why.
 extern "C" {
     // malloc.h
     pub fn free(p: *mut c_void);
     pub fn malloc(size: size_t) -> *mut c_void;
     pub fn calloc(nobj: size_t, size: size_t) -> *mut c_void;
     pub fn realloc(p: *mut c_void, size: size_t) -> *mut c_void;
-
-    // unistd.h
-    //pub fn write(fd: c_int, buf: *const c_void, count: size_t) -> ssize_t;
 
     // stdio.h
     pub fn printf(format: *const c_char, ...) -> c_int;
